f143d343d85cba1db6dda73f67458497f306b087
imnomee <59981447+imnomee@users.noreply.github.com>
2023-10-21 19:13:45 +0500

    timer to change quote and color
    Updated to exceed expectations.
    which changes the color automatically and the Quote after 5 seconds.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// For assistance:
-// Check the "Project Resources" section of the project instructions
-// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/***
- * `quotes` array
- ***/
-/*
-You must be the change you wish to see in the world. -Mahatma Gandhi
-Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa
-The only thing we have to fear is fear itself. -Franklin D. Roosevelt
-Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that. -Martin Luther King Jr.
-Do one thing every day that scares you. -Eleanor Roosevelt
-Well done is better than well said. -Benjamin Franklin
-The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller
-It is during our darkest moments that we must focus to see the light. -Aristotle
-Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson
-Be yourself; everyone else is already taken. -Oscar Wilde
-*/
 
 //random number function
 function getRandomNumber(upper) {
@@ -111,17 +87,12 @@ function printQuote(){
   console.log(quote.author);
   html += `
   <div id="quote-box" class="quote-box">
-    <p class="quote">${main}</p>
-    <p class="source">${auth}${cite}${year}</p>
+  <p class="quote">${main}</p>
+  <p class="source">${auth}${cite}${year}</p>
   </div>`
   container.innerHTML = html;
+  const bgColor = `rgb(${getRandomNumber(255)}, ${getRandomNumber(255)}, ${getRandomNumber(255)})`;
+  document.body.style.backgroundColor = bgColor;
 }
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
- ***/
-
-document
-  .getElementById("load-quote")
-  .addEventListener("click", printQuote, false);
+setInterval(printQuote, 5000);
